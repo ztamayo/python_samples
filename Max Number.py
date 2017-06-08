0fc4e60e230b63882d818c3fbebf12083601d708
Zailyn Tamayo <zailyn@gmail.com>
2017-06-08 14:38:01 -0700

    Add files via upload
    This program will prompt the user to enter two integers.  It will then output which is the larger number that was entered.  For example, if the user enters 4 and 8, the output will read "8 is the maximum".

---
+++ Max Number.py
@@ -0,0 +1,12 @@
+# Student: Zailyn Tamayo  # File Name: Exam1_2.py
+
+first = input("\nEnter the first integer: ")
+
+second = input("\nEnter the second integer: ")
+
+if first > second:
+  print ("\n", first, "is the maximum.")
+elif first == second:
+  print ("\nThey are equal.")
+else:
+  print ("\n", second, "is the maximum.")
